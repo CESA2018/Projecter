c7786c568239a70ff0aa4c65c66720cfca647491
Yuuma Murahata <spartan0606my@gmail.com>
2018-04-12 11:52:30 +0900

    オリジナル

---
+++ Assets/Scripts/ProjecterGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 鏡の弾を打ち出すクラス
+public class ProjecterGun : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject m_player;        //  プレイヤー
+
+    [SerializeField]
+    private GameObject m_bullet;        //  弾
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //  右クリックで弾を発射する
+        if (Input.GetMouseButtonDown(1))
+        {
+            //  弾を生成する
+            GameObject bullet = GameObject.Instantiate(m_bullet);
+            //  プレイヤーの位置に移動
+            bullet.transform.position = m_player.transform.position;
+            //  プレイヤーの向いてる角度に向ける
+            bullet.transform.rotation = m_player.transform.rotation;
+
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/StageChenge.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageChenge : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_player;
+
+    [SerializeField]
+    private GameObject m_respownArea;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // m_respownArea = GameObject.Find("RespownArea");
+
+            other.transform.position = m_respownArea.transform.position;
+
+            other.gameObject.GetComponent<UnityChanController>().ResetVelocity();
+
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //m_respownArea = GameObject.Find("RespownArea");
+
+            other.transform.position = m_respownArea.transform.position;
+
+            other.gameObject.GetComponent<UnityChanController>().ResetVelocity();
+        }
+
+    }
+}
