8c4f2346d55f858367139edc91c1b8f92b1b6c32
TSITCL\s162129 <taka1122judo@gmail.com>
2018-05-10 15:39:24 +0900

    プレイヤーの移動から加速と減速を削除

--- Assets/Scripts/nagaura/PlayerMoving.cs
+++ Assets/Scripts/nagaura/PlayerMoving.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class PlayerMoving : MonoBehaviour
 {
-    // Rigidbody
+    // 初期速度
     [SerializeField]
-    private Rigidbody m_rigidbody;
-    // メインカメラ
-    [SerializeField]
-    private GameObject m_camera;
-    // 移動速度
-    [SerializeField]
-    private float MOVING_SPEED;
+    private float INITIAL_SPEED;
     // 最高速度
     [SerializeField]
     private float MAXIMUM_SPEED;
@@ -25,6 +19,12 @@ public class PlayerMoving : MonoBehaviour
     // マウス感度
     [SerializeField]
     private float SENSITIVITY;
+    // Rigidbody
+    [SerializeField]
+    private Rigidbody m_rigidbody;
+    // メインカメラ
+    [SerializeField]
+    private GameObject m_camera;
     // 移動速度
     public Vector3 m_movingVelocity;
 
@@ -44,40 +44,46 @@ public class PlayerMoving : MonoBehaviour
 
     private void Moving()
     {
+        m_movingVelocity = Vector3.zero;
         // Wボタンを押下している
         if (Input.GetKey(KeyCode.W))
         {
-            m_movingVelocity += transform.forward * MOVING_SPEED;
+            m_movingVelocity += transform.forward * INITIAL_SPEED;
         }
         // Sボタンを押下している
         if (Input.GetKey(KeyCode.S))
         {
-            m_movingVelocity += -transform.forward * MOVING_SPEED;
+            m_movingVelocity += -transform.forward * INITIAL_SPEED;
         }
         // Dボタンを押下している
         if (Input.GetKey(KeyCode.D))
         {
-            m_movingVelocity += transform.right * MOVING_SPEED;
+            m_movingVelocity += transform.right * INITIAL_SPEED;
         }
         // Aボタンを押下している
         if (Input.GetKey(KeyCode.A))
         {
-            m_movingVelocity += -transform.right * MOVING_SPEED;
+            m_movingVelocity += -transform.right * INITIAL_SPEED;
         }
-        // 加速する
-        Accelerate();
-        // 減速する
-        Decelerate();
+        //// 加速する
+        //Accelerate();
+        //// 減速する
+        //Decelerate();
         // 速度を代入
-        //m_rigidbody.velocity = m_movingVelocity;
+        m_rigidbody.velocity = m_movingVelocity;
     }
 
     private void Accelerate()
     {
-        // Wキーを押下していて最高速度に達していないなら加速する
-        if (Input.GetKey(KeyCode.W) && m_movingVelocity.z > 0 && m_movingVelocity.z <= MAXIMUM_SPEED)
+        // WキーかSキーを押下していて最高速度に達していないなら加速する
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) && Mathf.Abs(m_movingVelocity.z) <= MAXIMUM_SPEED)
         {
             m_movingVelocity.z *= ACCELERATION;
+        }
+        // AキーかDキーを押下していて最高速度に達していないなら加速する
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && Mathf.Abs(m_movingVelocity.x) <= MAXIMUM_SPEED)
+        {
+            m_movingVelocity.x *= ACCELERATION;
         }
     }
     // 減速処理
@@ -91,7 +97,7 @@ public class PlayerMoving : MonoBehaviour
                 m_movingVelocity.z = 0f;
             }
         }
-        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S))
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
             m_movingVelocity.x *= DECELERATION;
             if (Mathf.Abs(m_movingVelocity.x) <= 0.01f)
